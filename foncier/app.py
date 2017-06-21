496cf323c06b2c618fc94b563b4311326a3b8282
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2017-06-21 14:44:53 +0200

    generic header url

--- foncier/app.py
+++ foncier/app.py
@@ -17,6 +17,7 @@ env=os.environ
 DEBUG = env.get('DEBUG', 'False')
 ROLE_PREFIX = env.get('ROLE_PREFIX', 'ROLE_FONCIER_')
 HEADER_HEIGHT = env.get('HEADER_HEIGHT', 90)
+HEADER_URL = env.get('HEADER_URL', '/header/')
 SORRY_PAGE_BODY = Markup(env.get('SORRY_PAGE_BODY', "<p>Cette application est réservée aux ayants droits PPIGE <a href='?login'>connectés</a> ayant signé un <a href='http://www.ppige-npdc.fr/portail/doc/Acte_Engagementdynamique2015_vF_cle6ca269.pdf'>acte d'engagement</a> en vue de la délivrance de fichiers fonciers.</p><p>Il semblerait que cette condition ne soit pas remplie pour votre compte, ou bien que le traitement de votre demande ne soit pas encore achevé.</p><p>Pour obtenir un accès à ces fichiers, vous devez obtenir l’autorisation de la Direction Générale de l'Aménagement, du Logement et de la Nature (DGALN). La procédure ci-dessous vous accompagne dans cette démarche.</p><p>Envoyer votre demande à la DGALN par un email remplissant les conditions suivantes :<ol><li>Objet du message : <b>[PPIGE] Demande de téléchargement de fichiers fonciers à partir du site de la PPIGE</b></li><li>Destinataire principal : <b>autorisations-fichiers-fonciers@developpement-durable.gouv.fr</b> </li><li>Destinataires en copie : <b>fichiers-fonciers@developpement-durable.gouv.fr, ppige@epf-npdc.fr</b></li><li>Pièce jointe : le <a href='http://www.ppige-npdc.fr/portail/doc/Acte_Engagementdynamique2015_vF_cle6ca269.pdf'>document DGALN-AD</a> dûment rempli. </li></ol></p><p>Votre demande sera traitée correctement et dans les plus brefs délais si vous respectez ces étapes.</p><p>Votre accès à l'extracteur des fichiers fonciers sera ouvert par la PPIGE dès réception de la validation de la DGALN.</p><p>Nous vous invitons à consulter également le document <a href='http://www.ppige-npdc.fr/portail/doc/AE-DGFIP-DGALN.pdf'>AE-DGFiP-DGALN</a></p>"))
 
 @app.before_request
@@ -35,7 +36,7 @@ def load_user():
         # get LDAP org object, extract description field with list of areas:
         g.cities = extract_cp(g.org)
     except ValueError:
-        return render_template('sorry.html', body=SORRY_PAGE_BODY, hheight=HEADER_HEIGHT)
+        return render_template('sorry.html', body=SORRY_PAGE_BODY, hheight=HEADER_HEIGHT, hurl=HEADER_URL)
     # store user roles & available millésimes
     prefix = ROLE_PREFIX
     rolesHeader = request.headers.get('sec-roles')
@@ -46,9 +47,9 @@ def load_user():
 @app.route('/', methods=['GET'])
 def index():
     if acces_foncier(g.roles) and len(g.cities) > 0:
-        return render_template('index.html', years=[str(y) for y in g.years], hheight=HEADER_HEIGHT)
+        return render_template('index.html', years=[str(y) for y in g.years], hheight=HEADER_HEIGHT, hurl=HEADER_URL)
     else:
-        return render_template('sorry.html', body=SORRY_PAGE_BODY, hheight=HEADER_HEIGHT)
+        return render_template('sorry.html', body=SORRY_PAGE_BODY, hheight=HEADER_HEIGHT, hurl=HEADER_URL)
 
 
 @app.route('/submit', methods=['POST'])
@@ -70,7 +71,7 @@ def submit():
         proj,
         g.email,
         g.cities], kwargs={})
-    return render_template('thanks.html', values=values, uuid=task.id, hheight=HEADER_HEIGHT)
+    return render_template('thanks.html', values=values, uuid=task.id, hheight=HEADER_HEIGHT, hurl=HEADER_URL)
 
 
 @app.route('/retrieve/<string:uuid>', methods=['GET'])
