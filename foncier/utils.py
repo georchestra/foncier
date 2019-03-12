db8b8fe54bf1583ed3d206a104801730f6176f21
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2019-03-12 10:55:08 +0100

    Adapt to LDAP new structure
    Pending orgs are defined differently than in 18.06 <
    There is no need anymore to filter on entry 'businessCategory'= 'REGISTERED'

--- foncier/utils.py
+++ foncier/utils.py
@@ -24,13 +24,9 @@ def extract_cp(org):
     cnx.search(search_base=LDAP_ORGS_BASEDN,
                search_filter=LDAP_SEARCH_FILTER % org,
                search_scope=LEVEL,
-               attributes=["businessCategory", "description"])
+               attributes=["description"])
 
     for entry in cnx.entries:
-        # check status of org
-        if entry['businessCategory'] != 'REGISTERED':
-            raise ValueError('Organism is still pending')
-
         if len(entry['description']) == 0:
             res = []
         else:
